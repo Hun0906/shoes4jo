af2070f270e3c7947d970fbf39be94899dee7bc0
Malg Eum Yu <134472216+yumalg12@users.noreply.github.com>
2023-08-04 14:57:50 +0900

    세라님 파일 적용

--- main/java/com/multi/shoes4jo/controller/LoginController.java
+++ main/java/com/multi/shoes4jo/controller/LoginController.java
@@ -1,3 +1,4 @@
+
 package com.multi.shoes4jo.controller;
 
 import javax.servlet.http.HttpServletRequest;
@@ -6,51 +7,48 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.multi.shoes4jo.dao.LoginDAO;
+import com.multi.shoes4jo.service.MemberService;
 import com.multi.shoes4jo.vo.MemberVO;
 
 @Controller
+
 @RequestMapping("/controller")
 public class LoginController {
-    
-    private final LoginDAO dao;
 
-    @Autowired
-    public LoginController(LoginDAO dao) {
-        this.dao = dao;
-    }
+	@Autowired
+	private MemberService memberService;
 
-	@RequestMapping(value = "/login", method = { RequestMethod.POST } )
+	@RequestMapping(value = "/login", method = { RequestMethod.POST })
 	public String login(MemberVO vo, Model model, HttpServletRequest request) {
-		String MemberID = request.getParameter("MEMBER_ID");
-		String MemberPW = request.getParameter("MEMBER_PW");
+		String memberID = request.getParameter("member_id");
+		String memberPW = request.getParameter("member_pw");
 
-        Integer loginRes = dao.login(MemberID,MemberPW);
+		vo.setmember_id(memberID);
+		vo.setmember_pw(memberPW);
+		Integer loginRes = memberService.loginMember(vo);
 
-        if (loginRes == 1) {
-            HttpSession session = request.getSession();
-            session.setAttribute("MemberID", MemberID);
-            
-            System.out.println(MemberID + " 계정 로그인 성공");
+		if (loginRes == 1) {
+			HttpSession session = request.getSession();
+			session.setAttribute("member_id", memberID);
 
-            model.addAttribute("res", loginRes);
-            return "redirect:/login";
-            } else { 
-            model.addAttribute("res", loginRes);
-            return "redirect:/login";
-        }
-    }
-	
-	@RequestMapping(value = "/logout", method = RequestMethod.GET)
-	public String logout(HttpServletRequest request, Model model) {
-	    HttpSession session = request.getSession();
-	    session.invalidate();
-	    model.addAttribute("res", 109);
-	    return "redirect:/login";
+			System.out.println(memberID + " 로그인 성공");
+
+			model.addAttribute("res", loginRes);
+			return "redirect:/login";
+		} else {
+			model.addAttribute("res", loginRes);
+			return "redirect:/login";
+		}
 	}
 
+	@RequestMapping(value = "/logout", method = RequestMethod.GET)
+	public String logout(HttpServletRequest request, Model model) {
+		HttpSession session = request.getSession();
+		session.invalidate();
+		model.addAttribute("res", 109);
+		return "redirect:/login";
+	}
 }
