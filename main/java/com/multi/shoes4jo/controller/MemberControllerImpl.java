1de74b54305a91dab97e54dcd1ac6147e9211d3f
이동훈 <134472292+Hun0906@users.noreply.github.com>
2023-08-10 15:46:24 +0900

    수정하기창 정보띄우고 수정내역 반영 ㅇㅇ

--- main/java/com/multi/shoes4jo/controller/MemberControllerImpl.java
+++ main/java/com/multi/shoes4jo/controller/MemberControllerImpl.java
@@ -53,35 +53,34 @@ public class MemberControllerImpl implements MemberController {
 	@Override
 	@RequestMapping(value = "/controller/login", method = { RequestMethod.POST, RequestMethod.GET })
 	public ModelAndView login(HttpServletRequest request, HttpServletResponse response) throws Exception {
-	    String memberID = request.getParameter("member_id");
-	    String memberPW = request.getParameter("member_pw");
+		String memberID = request.getParameter("member_id");
+		String memberPW = request.getParameter("member_pw");
 
-	    System.out.printf("member_id: %s, member_pw: %s\n", memberID, memberPW);
+		System.out.printf("member_id: %s, member_pw: %s\n", memberID, memberPW);
 
-	    MemberVO member = new MemberVO();
-	    member.setmember_id(memberID);
-	    member.setmember_pw(memberPW);
-	    Integer loginRes = memberService.loginMember(member);
+		MemberVO member = new MemberVO();
+		member.setmember_id(memberID);
+		member.setmember_pw(memberPW);
+		Integer loginRes = memberService.loginMember(member);
 
-	    ModelAndView mav = new ModelAndView();
-	    if (loginRes == 1) {
-	        HttpSession session = request.getSession();
-	        session.setAttribute("memberInfo", memberID);
+		ModelAndView mav = new ModelAndView();
+		if (loginRes == 1) {
+			HttpSession session = request.getSession();
+			session.setAttribute("memberInfo", memberID);
 
-	        System.out.println(memberID + " 로그인 성공");
+			System.out.println(memberID + " 로그인 성공");
 
-	        mav.setViewName("redirect:/login"); //alert를 띄우기 위해 main이 아니라 login으로 보내게 되어 있습니다.
-	        mav.addObject("res", 1); // 성공 코드
-	    } else if (loginRes == 0) {
-	        mav.setViewName("redirect:/login");
-	        mav.addObject("res", 0); // 비밀번호 오류 코드
-	    } else {
-	        mav.setViewName("redirect:/login");
-	        mav.addObject("res", -1); // 없는 아이디 코드
-	    }
-	    return mav;
+			mav.setViewName("redirect:/login"); // alert를 띄우기 위해 main이 아니라 login으로 보내게 되어 있습니다.
+			mav.addObject("res", 1); // 성공 코드
+		} else if (loginRes == 0) {
+			mav.setViewName("redirect:/login");
+			mav.addObject("res", 0); // 비밀번호 오류 코드
+		} else {
+			mav.setViewName("redirect:/login");
+			mav.addObject("res", -1); // 없는 아이디 코드
+		}
+		return mav;
 	}
-
 
 	@RequestMapping(value = "/controller/logout", method = { RequestMethod.POST, RequestMethod.GET })
 	public String logout(HttpServletRequest request, Model model) {
@@ -90,7 +89,7 @@ public class MemberControllerImpl implements MemberController {
 		model.addAttribute("res", 109);
 		return "redirect:/login";
 	}
- 
+
 	@Override
 	@RequestMapping(value = "/controller/memberInfo", method = { RequestMethod.POST, RequestMethod.GET })
 	public ModelAndView memberInfo(HttpServletRequest request, HttpServletResponse response) throws Exception {
@@ -106,32 +105,35 @@ public class MemberControllerImpl implements MemberController {
 		return mav;
 	}
 
+	
 	@Override
 	@RequestMapping(value = "/controller/my_edit", method = { RequestMethod.POST, RequestMethod.GET })
 	public ModelAndView updateMember(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		MemberVO member = new MemberVO();
-
-		String member_id = request.getParameter("member_id");
-		String member_name = request.getParameter("member_name");
-		String member_pw = request.getParameter("member_pw");
-		String member_email = request.getParameter("member_email");
-		String member_phone = request.getParameter("member_phone");
-
-		member.setmember_id(member_id);
-		member.setmember_name(member_name);
-		member.setmember_pw(member_pw);
-		member.setmember_email(member_email);
-		member.setmember_phone(member_phone);
-
-		memberService.updateMember(member);
-
 		HttpSession session = request.getSession();
-		session.invalidate();
-
-		return new ModelAndView("redirect:/my_edit");
+		String member_id = (String) session.getAttribute("memberInfo");
+		String member_name = request.getParameter("member_name");
+	    String member_pw = request.getParameter("member_pw");
+	    String member_email = request.getParameter("member_email");
+	    String member_phone = request.getParameter("member_phone");
+	    
+	    MemberVO updatedMember = new MemberVO();
+	    updatedMember.setmember_id(member_id);
+	    updatedMember.setmember_name(member_name);
+	    updatedMember.setmember_pw(member_pw);
+	    updatedMember.setmember_email(member_email);
+	    updatedMember.setmember_phone(member_phone);
+	    
+	    memberService.updateMember(updatedMember);
+	    
+		MemberVO updatedMemberInfo = memberService.memberInfo(member_id);
+		
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("member/my_edit");
+		mav.addObject("memberInfo",updatedMemberInfo);
+		mav.addObject("updateSucess", true);
+		
+		return mav;
 	}
-
-
 
 	@Override
 	@ResponseBody
@@ -145,16 +147,14 @@ public class MemberControllerImpl implements MemberController {
 
 		ModelAndView mav = new ModelAndView();
 		if (result > 0) {
-		    HttpSession session = request.getSession();
-		    session.invalidate();
-		    mav.setViewName("redirect:/login");
+			HttpSession session = request.getSession();
+			session.setAttribute("memberInfo", member_id);
+			mav.setViewName("redirect:/login");
 		} else {
-		    mav.setViewName("member/member_delete");
+			mav.setViewName("member/member_delete");
 		}
 		return mav;
 	}
-	
-	
 
 	@Override
 	@RequestMapping("/duplicationId")
