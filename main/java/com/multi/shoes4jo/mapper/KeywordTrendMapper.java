25c5831a092d916884cff8b3bd20fa8a25f45db3
0123aqq@gmail.com <0123aqq@gmail.com>
2023-09-05 15:26:32 +0900

    테이블명 바꾼거에 맞춘거

--- main/java/com/multi/shoes4jo/mapper/KeywordTrendMapper.java
+++ main/java/com/multi/shoes4jo/mapper/KeywordTrendMapper.java
@@ -13,30 +13,30 @@ import com.multi.shoes4jo.vo.KeywordTrendVO;
 @Mapper
 public interface KeywordTrendMapper {
 	//list
-    @Select("SELECT * FROM `4jo_keyword_trend` WHERE keyword = #{keyword} ORDER BY query_value DESC limit 10")
+    @Select("SELECT * FROM `keyword_trend` WHERE keyword = #{keyword} ORDER BY query_value DESC limit 10")
     List<KeywordTrendVO> selectKeyword(@Param("keyword") String keyword);
     
-    @Select("SELECT `keyword` FROM `4jo_keyword_trend` WHERE keyword_group = #{keyword_group}")
+    @Select("SELECT `keyword` FROM `keyword_trend` WHERE keyword_group = #{keyword_group}")
     List<String> selectGroup(@Param("keyword_group") String keyword_group);
     
     
     //isExists
-    @Select("SELECT count(*) FROM `4jo_keyword_trend` WHERE keyword = #{keyword} and query = #{query}")
+    @Select("SELECT count(*) FROM `keyword_trend` WHERE keyword = #{keyword} and query = #{query}")
     int isExists(@Param("keyword") String keyword, @Param("query") String query);
     
     
     //oldValue
-    @Select("SELECT query_value FROM `4jo_keyword_trend` WHERE keyword = #{keyword} and query = #{query}")
+    @Select("SELECT query_value FROM `keyword_trend` WHERE keyword = #{keyword} and query = #{query}")
     int oldValue(@Param("keyword") String keyword, @Param("query") String query);
 
     
     //insert
-    @Insert("INSERT INTO `4jo_keyword_trend` (keyword_group, keyword, query, query_value) VALUES (#{keyword_group}, #{keyword}, #{query}, #{query_value})")
+    @Insert("INSERT INTO `keyword_trend` (keyword_group, keyword, query, query_value) VALUES (#{keyword_group}, #{keyword}, #{query}, #{query_value})")
     void insert(KeywordTrendVO vo);
 
     
     //update
-    @Update("UPDATE `4jo_keyword_trend` SET query_value = #{query_value} WHERE keyword = #{keyword} AND query = #{query}")
+    @Update("UPDATE `keyword_trend` SET query_value = #{query_value} WHERE keyword = #{keyword} AND query = #{query}")
     void update(KeywordTrendVO vo);
 
 }
