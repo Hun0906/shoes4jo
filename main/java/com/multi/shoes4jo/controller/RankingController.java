00580323f1b82729a9523cdcf4bfb1e29c6deff6
Malg Eum Yu <134472216+yumalg12@users.noreply.github.com>
2023-09-06 09:54:28 +0900

    랭킹 페이지 디자인 및 작동 쿼리문 수정

--- main/java/com/multi/shoes4jo/controller/RankingController.java
+++ main/java/com/multi/shoes4jo/controller/RankingController.java
@@ -1,6 +1,9 @@
 package com.multi.shoes4jo.controller;
 
+import java.time.LocalDate;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -21,10 +24,25 @@ public class RankingController {
 	@RequestMapping(value = "/ranking.do")
 	public String list(Model model, Criteria cri) throws Exception {
 
-		List<RankingVO> list;
-		list = service.searchRanking();
+		List<RankingVO> dayRank;
+		List<RankingVO> weekRank;
+		List<RankingVO> monthRank;
+		
+		LocalDate today = LocalDate.now();
+		dayRank = service.selectTopTen(today.toString(), 10);
+		weekRank = service.selectTopTen(today.minusWeeks(1).toString(), 10);
+		monthRank = service.selectTopTen(today.minusMonths(1).toString(), 10);
 
-		model.addAttribute("list", list);
+		model.addAttribute("dayRank", dayRank);
+		model.addAttribute("weekRank", weekRank);
+		model.addAttribute("monthRank", monthRank);
+		
+		Map<String, String> dateMap = new HashMap<>();
+		dateMap.put("today", today.toString().replaceAll("-", "/"));
+		dateMap.put("week", today.minusWeeks(1).toString().replaceAll("-", "/"));
+		dateMap.put("month", today.minusMonths(1).toString().replaceAll("-", "/"));
+
+		model.addAttribute("dateMap", dateMap);
 
 		return "trend/ranking";
 	}
