c6058a90984dc256565b2abd4d9d67c31cd73955
0123aqq@gmail.com <0123aqq@gmail.com>
2023-09-13 02:01:29 +0900

    no message

--- main/java/com/multi/shoes4jo/freeboard/FreeBoardController.java
+++ main/java/com/multi/shoes4jo/freeboard/FreeBoardController.java
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.multi.shoes4jo.board.BoardVO;
 import com.multi.shoes4jo.util.Criteria;
 import com.multi.shoes4jo.util.PageMaker;
 
